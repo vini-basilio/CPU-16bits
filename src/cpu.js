7dd4e69910d0dde50cd52d28b8c9c7a4846a5cae
Vini <vini.ba.dev@gmail.com>
2025-06-06 13:54:54 -0300

    - feat: adiciona - soma - subtracao - incremento e decremento de regs - multiplicao para numeros sem sinal

--- src/cpu.js
+++ src/cpu.js
@@ -1,27 +1,27 @@
-import { hexToAssembly, instructions } from "./instructions.js";
+import {hexToAssembly, instructions} from "./instructions.js";
 import {createMemory} from "./create-memory.js";
-import { brotliCompressSync } from "node:zlib";
+import {brotliCompressSync} from "node:zlib";
 
 class CPU {
-    constructor(memory){
+    constructor(memory) {
         this.memory = memory;
 
         // Registradores publicos
         this.registersNames =
-        [
-           "ip", "acc", 
-           "r1", "r2", "r3","r4", 
-           "r5", "r6", "r7","r8",
-           "sp","fp",
-        ]
+            [
+                "ip", "acc",
+                "r1", "r2", "r3", "r4",
+                "r5", "r6", "r7", "r8",
+                "sp", "fp",
+            ];
 
         // cria uma memoria de 16 bits para cada reg que será todos os reg que temos
         // esse é a palavra, logo, o nome do processador
         this.registers = createMemory(this.registersNames.length * 2);
 
         this.registersMap = this.registersNames.reduce((map, name, i) => {
-            map[name] = i * 2 
-            return map
+                map[name] = i * 2;
+                return map;
             }, {}
         );
 
@@ -31,120 +31,90 @@ class CPU {
         this.setRegister("fp", 0xffff - 1);
 
 
-
         //Registradores privados
         this.stackFrameSize = 0;
 
 
     }
-    debug(){
+
+    debug() {
         this.registersNames.forEach(name => {
-            console.log(`${name}: ${this.getRegister(name).toString(16).padStart(4,0)}`)  
-        })
-        console.log(" ")
+            console.log(`${name}: ${this.getRegister(name).toString(16).padStart(4, 0)}`);
+        });
+        console.log(" ");
     }
 
     // para debug tambem
-    viewMemoryAt(address, n = 8){
+    viewMemoryAt(address, n = 8) {
         /* 
             As vezes nossa CPU lida com os dados como 2 bytes
             As vezes como 16 bit, uma palavra 
         */
 
-            const nextNtBytes = Array.from({length: n}, 
-                (_, i) => this.memory.getUint8(address + i))
-                .map( v => v.toString(16).padStart(4, '0x'));
+        const nextNtBytes = Array.from({length: n},
+            (_, i) => this.memory.getUint8(address + i))
+            .map(v => v.toString(16).padStart(4, "0x"));
 
-        console.log(`         Current address: 0x${address.toString(16).padStart(4, '0')}
-         Content: ${nextNtBytes.join(' ')}`); 
+        console.log(`         Current address: 0x${address.toString(16).padStart(4, "0")}
+         Content: ${nextNtBytes.join(" ")}`);
     }
-    assembly(address){
-     
-            let encoded = this.memory.getUint8(address);
-            const decoded = hexToAssembly.get(encoded.toString(16).padStart(4, '0x'))
 
-            switch(decoded){
-                case "MOV_LIT_REG": {
-                    address += 1;
-                    const value = this.memory.getUint16(address);
-                    address += 2
-                    const reg = this.memory.getUint8(address);
-                    console.log(`
-                    OP: MOV LIT REG 
-                    LIT: ${value.toString(16)} 
-                    REG ${this.registersNames[reg.toString(16) % this.registersNames.length]}
-                    `)
-                    break;
-                }
-                case "MOV_MEM_REG": {
-                    address += 1;
-                    const value = this.memory.getUint16(address);
-                    address += 2
-                    const reg = this.memory.getUint8(address);
-                    console.log(`
-                    OP: MOV MEM REG 
-                    VALUE FROM ADDR: ${value.toString(16).padStart(4, '0x')} 
-                    REG ${this.registersNames[reg.toString(16) % this.registersNames.length]}
-                    `)
-                    break;
-                }
-                default: {
-                    
-                }
-
-            }
-    }
     // OP com REG
-    getRegister(name){
-        if(!(name in this.registersMap)){
-            throw new Error(`getRegister: No such register '${name}'`)
+    getRegister(name) {
+        if (!(name in this.registersMap)) {
+            throw new Error(`getRegister: No such register '${name}'`);
         }
         return this.registers.getUint16(this.registersMap[name]);
-    } 
-    setRegister(name, value){
-        if(!(name in this.registersMap)){
-            throw new Error(`setRegister: No such register '${name}'`)
+    }
+
+    setRegister(name, value) {
+        if (!(name in this.registersMap)) {
+            throw new Error(`setRegister: No such register '${name}'`);
         }
         return this.registers.setUint16(this.registersMap[name], value);
     }
+
     // OP de busca
-    fetch(){
+    fetch() {
         const nextInstructionAddress = this.getRegister("ip");
         const instruction = this.memory.getUint8(nextInstructionAddress);
         this.setRegister("ip", nextInstructionAddress + 1);
         return instruction;
     }
-    fetch16(){
+
+    fetch16() {
         const nextInstructionAddress = this.getRegister("ip");
         const instruction = this.memory.getUint16(nextInstructionAddress);
         this.setRegister("ip", nextInstructionAddress + 2);
         return instruction;
     }
-    fetchRegisterIndex(){
+
+    fetchRegisterIndex() {
         return (this.fetch() % this.registersNames.length) * 2;
     }
 
     //OP da Stacks
     //Mover o ponteiro
-    push(value){
+    push(value) {
         const spAddress = this.getRegister("sp");
         this.memory.setUint16(spAddress, value);
         this.setRegister("sp", spAddress - 2);
         this.stackFrameSize += 2;
     }
 
-    pop(){
+    pop() {
         const nextSpAddress = this.getRegister("sp") + 2;
         this.setRegister("sp", nextSpAddress);
         this.stackFrameSize -= 2;
         return this.memory.getUint16(nextSpAddress);
     }
+
     // Salvar o estado do processador para subrotinas
-    pushState(){
+    pushState() {
         // Atualiza a stack com o que estava nos registradores
         // Como a stack fica na RAM, estamos salvando um estado
         // Isso não é o mesmo que salvar o Contexto
-        for(let i = 2; i < 10; i++){
+        for (let i = 2; i < 10; i++) {
             // console.log(this.registersNames[i])
             this.push(this.getRegister(this.registersNames[i]));
 
@@ -155,34 +125,35 @@ class CPU {
         this.setRegister("fp", this.getRegister("sp"));
         this.stackFrameSize = 0;
     }
+
     popState() {
-        const framePointerAddress = this.getRegister('fp');
-        this.setRegister('sp', framePointerAddress);
-    
+        const framePointerAddress = this.getRegister("fp");
+        this.setRegister("sp", framePointerAddress);
+
         this.stackFrameSize = this.pop();
         const stackFrameSize = this.stackFrameSize;
-    
-        this.setRegister('ip', this.pop());
-        this.setRegister('r8', this.pop());
-        this.setRegister('r7', this.pop());
-        this.setRegister('r6', this.pop());
-        this.setRegister('r5', this.pop());
-        this.setRegister('r4', this.pop());
-        this.setRegister('r3', this.pop());
-        this.setRegister('r2', this.pop());
-        this.setRegister('r1', this.pop());
-    
+
+        this.setRegister("ip", this.pop());
+        this.setRegister("r8", this.pop());
+        this.setRegister("r7", this.pop());
+        this.setRegister("r6", this.pop());
+        this.setRegister("r5", this.pop());
+        this.setRegister("r4", this.pop());
+        this.setRegister("r3", this.pop());
+        this.setRegister("r2", this.pop());
+        this.setRegister("r1", this.pop());
+
         const nArgs = this.pop();
         for (let i = 0; i < nArgs; i++) {
-          this.pop();
+            this.pop();
         }
-    
-        this.setRegister('fp', framePointerAddress + stackFrameSize);
-      }
+
+        this.setRegister("fp", framePointerAddress + stackFrameSize);
+    }
 
 
-    execute(instruction){
-        switch(instruction){
+    execute(instruction) {
+        switch (instruction) {
             /*
                 Move um valor para o registrador
 
@@ -214,10 +185,10 @@ class CPU {
                 const registerFrom = this.fetchRegisterIndex();
                 const registerTo = this.fetchRegisterIndex();
                 const value = this.registers.getUint16(registerFrom);
-                this.register.setUint16(registerTo, value);
+                this.registers.setUint16(registerTo, value);
                 break;
             }
-            
+
             case instructions.MOV_REG_MEM: {
                 const registerFrom = this.fetchRegisterIndex();
                 const address = this.fetch16();
@@ -232,20 +203,107 @@ class CPU {
                 this.registers.setUint16(registerTo, value);
                 break;
             }
+            case instructions.MOV_LIT_MEM: {
+                const value = this.fetch16();
+                const address = this.fetch16();
+                this.memory.setUint16(value);
+                break;
+            }
+            // Ponteiro de registrador
+            case instructions.MOV_REG_PTR_REG: {
+                const r1 = this.fetchRegisterIndex();
+                const r2 = this.fetchRegisterIndex();
+                const ptr = this.registers.getUint16(r1);
+                const value = this.memory.getUint16(ptr);
+                this.registers.setUint16(r2, value);
+                break;
+            }
+            // Move um valor [literal + registrador] para registrador
+            case instructions.MOV_LIT_OFF_REG: {
+                const baseAddress = this.fetch();
+                const r1 = this.fetchRegisterIndex();
+                const r2 = this.fetchRegisterIndex();
+                const offset = this.registers.getUint16(r1);
+                const value = this.memory.getUint16(baseAddress + offset);
+                this.registers.setUint16(r2, value);
+                break;
+            }
             case instructions.ADD_REG_REG: {
-                const r1 = this.fetch();
-                const r2 = this.fetch();
+                const r1 = this.fetchRegisterIndex();
+                const r2 = this.fetchRegisterIndex();
 
                 const reg1 = this.registers.getUint16(r1);
                 const reg2 = this.registers.getUint16(r2);
                 this.setRegister("acc", reg1 + reg2);
                 break;
             }
+            case instructions.ADD_LIT_REG: {
+                const literal = this.fetch16();
+                const r1 = this.fetchRegisterIndex();
+                const registerValue = this.registers.getUint16(r1);
+                this.setRegister("acc", literal + registerValue);
+                break;
+            }
+            case instructions.SUB_LIT_REG: {
+                const literal = this.fetch16();
+                const r1 = this.fetchRegisterIndex();
+                const registerValue = this.registers.getUint16(r1);
+                this.setRegister("acc", registerValue - literal);
+                break;
+            }
+            case instructions.SUB_REG_LIT: {
+                const literal = this.fetch16();
+                const r1 = this.fetchRegisterIndex();
+                const registerValue = this.registers.getUint16(r1);
+                this.setRegister("acc", literal - registerValue);
+                break;
+            }
+            case instructions.SUB_REG_REG: {
+                const r1 = this.fetchRegisterIndex();
+                const r2 = this.fetchRegisterIndex();
+
+                const reg1 = this.registers.getUint16(r1);
+                const reg2 = this.registers.getUint16(r2);
+
+                this.setRegister("acc", reg1 - reg2);
+                break;
+            }
+            case instructions.MUL_LIT_REG: {
+                const literal = this.fetch16();
+                const r1 = this.fetchRegisterIndex();
+                const registerValue = this.registers.getUint16(r1);
+                this.setRegister("acc", registerValue * r1);
+                break;
+            }
+            case instructions.MUL_REG_REG: {
+                const r1 = this.fetchRegisterIndex();
+                const r2 = this.fetchRegisterIndex();
+
+                const reg1 = this.registers.getUint16(r1);
+                const reg2 = this.registers.getUint16(r2);
+
+                this.setRegister("acc", reg2 * reg1);
+                break;
+            }
+            case instructions.INC_REG : {
+                const r1 = this.fetchRegisterIndex();
+                const oldValue = this.registers.getUint16(r1);
+                const newValue = oldValue + 1;
+                this.registers.setUint16(r1, newValue);
+                break;
+            }
+            case instructions.DEC_REG : {
+                const r1 = this.fetchRegisterIndex();
+                const oldValue = this.registers.getUint16(r1);
+                const newValue = oldValue - 1;
+                this.registers.setUint16(r1, newValue);
+                break;
+            }
             case instructions.JMP_NOT_EQ: {
                 const value = this.fetch16();
                 const address = this.fetch16();
 
-                if(value != this.getRegister("acc")){
+                if (value !== this.getRegister("acc")) {
                     this.setRegister("ip", address);
                 }
                 break;
@@ -262,7 +320,7 @@ class CPU {
             }
             case instructions.POP: {
                 const registerIndex = this.fetchRegisterIndex();
-                const value = this.pop()
+                const value = this.pop();
                 this.registers.setUint16(registerIndex, value);
                 break;
             }
@@ -285,19 +343,21 @@ class CPU {
                 this.popState();
                 break;
             }
-            case instructions.HLT:{
+            case instructions.HLT: {
                 return true;
             }
-        
+
         }
     }
-    step(){
+
+    step() {
         const instruction = this.fetch();
         return this.execute(instruction);
     }
-    run(){
+
+    run() {
         const halt = this.step();
-        if(!halt) {
+        if (!halt) {
             setImmediate(() => this.run());
         }
     }
--- src/instructions.js
+++ src/instructions.js
@@ -1,27 +1,39 @@
 const instructions = {
-    // Manda um enredeco para um REG especifico
     MOV_LIT_REG: 0x10,
-    MOV_REG_REG: 0x11, 
-    MOV_REG_MEM: 0x12, 
-    MOV_MEM_REG: 0x13, 
+    MOV_REG_REG: 0x11,
+    MOV_REG_MEM: 0x12,
+    MOV_LIT_MEM: 0x1B,
+    MOV_REG_PTR_REG: 0x1C,
+    MOV_LIT_OFF_REG: 0x1D,
+    MOV_MEM_REG: 0x13,
+
     ADD_REG_REG: 0x14,
-    JMP_NOT_EQ:  0x15,
-    PSH_LIT:     0x17,
-    PSH_REG:     0x18,
-    POP:         0x1A,
+    ADD_LIT_REG: 0x3F,
+    SUB_LIT_REG: 0x16,
+    SUB_REG_LIT: 0x1E,
+    SUB_REG_REG: 0x1F,
+    INC_REG: 0x35,
+    DEC_REG: 0x36,
+    MUL_LIT_REG: 0x20,
+    MUL_REG_REG: 0x21,
+
+    JMP_NOT_EQ: 0x15,
+    PSH_LIT: 0x17,
+    PSH_REG: 0x18,
+    POP: 0x1A,
     // Chama subrotina de um endereco na memoria
-    CAL_LIT:     0x5E,
+    CAL_LIT: 0x5E,
     // Chama subrotina de um REG
-    CAL_REG:     0x5F,
+    CAL_REG: 0x5F,
     // Informa que a subrotina terminou
-    RET:         0x06,
-    HLT:         0xFF,
-}
+    RET: 0x06,
+    HLT: 0xFF,
+};
 const hexToAssembly = Object.entries(instructions).reduce((map, element) => {
-    const value = element[1].toString(16).padStart(4, "0x" );
+    const value = element[1].toString(16).padStart(4, "0x");
     const name = element[0];
     return map.set(value, name);
 }, new Map());
 
 
-export { instructions, hexToAssembly }
+export {instructions, hexToAssembly};
